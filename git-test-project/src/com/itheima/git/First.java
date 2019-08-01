446fc8576a91e5dce5ac312d199949592f77087c
zhangsan <1123330160@qq.com>
2019-08-01 09:48:20 +0800

    修改了一下bug

--- git-test-project/src/com/itheima/git/First.java
+++ git-test-project/src/com/itheima/git/First.java
@@ -4,4 +4,8 @@ public class First {
     public static void main(String[] args) {
         System.out.println("hello git");
     }
+
+    public void test(){
+        System.out.println("第一次修订");
+    }
 }
